297e7ba6b7603b6bf9430b320de7530401437e8f
gstvcampos <gustavo.bmc76@gmail.com>
2023-02-23 14:34:45 -0300

    bug

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-// Implemente aqui as funções
+// testar altura
+function heightTest(gender, height) {
+    if (((gender == "male") && (height >= 1.70)) || // Teste masculino
+    ((gender == "female") && (height >= 1.60))) { // Teste feminino
+        return true
+    }
+}
+// teste barra
+function barTest(gender, barReps, barSeconds) {
+    if (((gender == "male") && (barReps >= 6 || barSeconds <= 15)) || //Teste masculino
+    ((gender == "female") && (barReps >= 5 || barSeconds <= 12))) { //Teste feminino
+        return true
+    }
+}
+// teste abdominais
+function absTest(abs) {
+    if (abs >= 41) {
+        return true
+    }
+}
+// teste corrida
+function runTest(gender, runDistance, runTime) {
+    if (((gender == "male") && ((runDistance >= 3000 && runTime <= 720) || (runDistance >= 5000 && runTime <= 1200))) || 
+    (gender == "female") && ((runDistance >= 4000 && runTime <= 900) || (runDistance >= 6000 && runTime <= 1320))) {
+        return true
+    }
+}
+// teste natação
+function swimTest(swimDistance, swimTime, diveTime) {
+    if ((swimDistance >= 100 && swimTime <= 60) || (diveTime <= 30)) {
+        return true
+    }
+}
+// caixa alta
+function showMessage(message) {
+    console.log(message.toUpperCase());
+}
 
-
-
-
-
-function areCandidateResultsValid() {
-
+function areCandidateResultsValid(gender, height, barReps, barSeconds, abs, runDistance, runTime, swimDistance, swimTime, diveTime) { 
+    if (
+        heightTest(gender, height) && 
+        barTest(gender, barReps, barSeconds) && 
+        absTest(abs) && 
+        runTest(gender, runDistance, runTime) && 
+        swimTest(swimDistance, swimTime, diveTime)
+        ) {
+        return true
+    }
+    return false
 }
 
 // Chamando a função com os parametros recebidos via script de teste
@@ -23,4 +64,10 @@ const areCandidateValid = areCandidateResultsValid(
     process.argv[11], // Tempo total de mergulho em segundos
 );
 
+areCandidateResultsValid()
 // Chame aqui a função que mostra o resultado no console
+if (areCandidateResultsValid()) {
+    showMessage("true")
+} else {
+    showMessage("false")
+}
